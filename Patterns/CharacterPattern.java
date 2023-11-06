4605619c9f8ec91bb0a4e366f1782d6776e2bbe9
mohitydv11 <128322737+mohitydv11@users.noreply.github.com>
2023-11-06 21:10:46 +0530

    Add files via upload

---
+++ Patterns/CharacterPattern.java
@@ -0,0 +1,19 @@
+package Patterns;
+import java.util.*;
+public class CharacterPattern{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        char ch='A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                    System.out.print(ch);
+                    ch++;
+            }
+            System.out.println();
+            
+        }
+
+    }
+    
+}
---
+++ Patterns/HalfPyramid.java
@@ -0,0 +1,17 @@
+package Patterns;
+import java.util.*;
+public class HalfPyramid{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                    System.out.print(j);
+            }
+            System.out.println();
+            
+        }
+
+    }
+    
+}
